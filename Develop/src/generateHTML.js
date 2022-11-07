8a604e2830942adc65b7e1e5e7de3eeedbf88c24
Ian <0306pitt@gmail.com>
2022-11-06 19:43:03 -0500

    generateTeamPage added

--- Develop/src/generateHTML.js
+++ Develop/src/generateHTML.js
@@ -3,6 +3,7 @@ const generateTeam = team => {
     // create the manager html
     const generateManager = manager => {
         return `
+
         <div class="info">
         <li>
         <h2>${manager.name}</h2>
@@ -36,6 +37,7 @@ const generateTeam = team => {
     // create the html for interns
     const generateIntern = intern => {
         return `
+
         <div class="info">
         <li>
         <h2>${intern.name}</h2>
@@ -49,8 +51,62 @@ const generateTeam = team => {
         `;
     };
 
+    const generateIntern = IT => {
+        return `
+
+        <div class="info">
+        <li>
+        <h2>${iT.name}</h2>
+        <h3>Engineer</h3>
+        <p class="email">Email: <a href="${iT.email}"></a>${iT.email}</p>
+        <p class="id">Id: ${iT.id}</p>
+        <p class="gitHub">GitHub: ${iT.gitHub}</p>
+        </li>
+      </div>
+
+        `;
+    };
+
     // use the team array to generate pieces of html based on the employee role
 }
+
+const generateTeamPage = function (employeeCards) {
+    return `
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="reset.css" />
+    <link rel="stylesheet" href="style.css" />
+    <title>My Team</title>
+  </head>
+
+  <body>
+
+    <header>
+      Team Profile
+    </header>
+
+    <main>
+      <div class="container">
+        <div class="info" id="cards">
+
+          <ul>
+            ${employeeCards}
+          </ul>
+
+        </div>
+      </div>
+    </main>
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  
+  </body>
+  </html>
+  `;
+  };
 
 module.exports = team => {
     // template literal - html body that calls the generate team function
